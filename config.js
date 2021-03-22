feda071f80856547660cc487a57cedf3f774c9fa
Jamal Rizki <jamalbrizki@gmail.com>
2021-03-22 19:53:13 -0400

    gitignorefiles

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '453996019254047',
+        clientSecret: 'c234e524d7e2690484fa2adacb884838'
+    }
 }
