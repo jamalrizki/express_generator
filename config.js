fe80ce0497687f09e015d1d04f86e3a25fed72e2
Jamal Rizki <jamalbrizki@gmail.com>
2021-03-22 20:01:02 -0400

    new

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '453996019254047',
-        clientSecret: 'c234e524d7e2690484fa2adacb884838'
-    }
-}
